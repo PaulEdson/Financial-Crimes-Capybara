305bfa9671a2dc7804db3b5f7c8b769ba59a74f5
Miranda Liu <miranda.liu8@gmail.com>
2024-11-14 14:24:28 -0600

    generated form modules

--- src/project/src/app.module.ts
+++ src/project/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthgroupModule } from './authgroup/authgroup.module';
 import { AuthorizationModule } from './authorization/authorization.module';
+import { FormModule } from './form/form.module';
 
 @Module({
-  imports: [UserModule, AuthgroupModule, AuthorizationModule],
+  imports: [UserModule, AuthgroupModule, AuthorizationModule, FormModule],
   controllers: [AppController],
   providers: [AppService],
 })
