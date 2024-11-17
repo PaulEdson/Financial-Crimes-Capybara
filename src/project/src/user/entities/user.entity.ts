9b278e293a229bea4d5727fca2ddd684c5edc5c8
Miranda Liu <miranda.liu8@gmail.com>
2024-11-17 13:26:28 -0800

    defined user entity

--- src/project/src/user/entities/user.entity.ts
+++ src/project/src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    userId: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    // @ManyToMany()
+    // @JoinTable()
+    // forms: Form[];
+}
